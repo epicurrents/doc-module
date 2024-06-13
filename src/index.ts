7768ac2dc544867a095bd2023021419fc8564f27
sam-19 <26563023+sam-19@users.noreply.github.com>
2024-06-13 07:01:57 +0300

    Export loader and service classes
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import HtmDocument from './HtmDocument'
+import HtmService from './service/HtmService'
+import HtmStudyLoader from './loader/HtmStudyLoader'
 import runtime from './runtime'
 import settings from './config'
 
 export {
     HtmDocument,
+    HtmService,
+    HtmStudyLoader,
     runtime,
     settings,
 }
